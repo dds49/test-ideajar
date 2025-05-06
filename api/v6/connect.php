1ba68f53567435502c9afab22e1952072234beab
dds49 <dds49@live.com>
2025-05-06 18:04:48 +0200

    No v6 API

--- api/v6/connect.php
+++
@@ -1,9 +0,0 @@
-<?php
-$conn=mysqli_connect("mysql5035.site4now.net","a1397e_ideajar","shinji86","db_a1397e_ideajar");
-   
-if (mysqli_connect_errno()) 
-    {
-      echo "Failed to connect to MySQL: " . mysqli_connect_error();
-  exit();
-      
-    }
