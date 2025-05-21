d4ca19dfc1249aa5b6b0c8c674446e2bc61a6fcc
dds49 <dds49@live.com>
2025-05-21 19:17:01 +0200

    Initial

--- src/App.jsx
+++ src/App.jsx
@@ -1,94 +1,61 @@
-import React from "react";
-import { useState } from "react";
-import axios from "axios";
-//import { Circles } from "react-loader-spinner";
-
+import { useState, useEffect } from "react";
 import "./App.css";
 import ListIdeas from "./components/ListIdeas";
-import ViewIdea from "./components/ViewIdea";
-//import ideas from "./data/jar.json";
 
 function App() {
-  const [ideas, setIdeas] = useState([]);
-  const [description, setDescription] = useState("");
-  // eslint-disable-next-line no-unused-vars
-  const [count, setCount] = useState(0);
-  const [view, setView] = useState(false);
   const [text, setText] = useState("");
 
-  // Try to get the jar.json file from the server
-  axios.get("http://ideajar.paranom.asia/api/getjson.php").then(
-    (response) => {
-      setIdeas(response.data);
-      setCount(response.data.length);
-      //console.log("count= " + count);
-    },
-    (error) => {
-      console.log(error);
-    }
-  );
-  //const [loading, setLoading] = useState(false);
+  const [jsonData, setJsonData] = useState(null);
+  const [count, setCount] = useState(0);
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    if (description.length < 1) null;
-    else {
-      //setLoading(true);
-      // Add new idea to JSON file 'jar.json'
-      const idea_obj = {
-        description: description,
-      };
-      ideas.push(idea_obj);
-      // Try to send the jar.json file to the server
-      axios.post("http://ideajar.paranom.asia/api/postjson.php", ideas);
-      setCount(ideas.length);
-      setDescription("");
-      //setLoading(false);
-    }
+  const getJsonData = async () => {
+    const response = await fetch("/data/jar.json", {
+      cache: "no-cache",
+    });
+    const data = await response.json();
+    setCount(data.length);
+    return data;
   };
 
+  useEffect(() => {
+    console.log(count);
+    getJsonData().then((data) => {
+      setJsonData(data);
+    });
+  }, [count]);
+
+  if (jsonData === null) {
+    return null;
+  }
+
+  function shuffle(array) {
+    let currentIndex = array.length;
+
+    // While there remain elements to shuffle...
+    while (currentIndex != 0) {
+      // Pick a remaining element...
+      let randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
+
+      // And swap it with the current element.
+      [array[currentIndex], array[randomIndex]] = [
+        array[randomIndex],
+        array[currentIndex],
+      ];
+    }
+  }
+
+  shuffle(jsonData);
+
   // RENDER
-  if (view) return <ViewIdea text={text} setView={setView} />;
-  else
-    return (
-      <>
-        <div>
-          <h1>IDEAS JAR</h1>
-          <form onSubmit={handleSubmit}>
-            <label className="form">Enter your idea:&nbsp;&nbsp;</label>
-            <input
-              type="text"
-              maxLength={128}
-              value={description}
-              onChange={(e) => setDescription(e.target.value)}
-            />
-            &nbsp;&nbsp;
-            <input type="submit" value="Go!" />
-          </form>
-        </div>
-        <ListIdeas
-          ideas={ideas}
-          view={view}
-          setView={setView}
-          text={text}
-          setText={setText}
-        />
-      </>
-    );
-  /*if (loading)
-    return (
-      <div className="loading">
-        <h1>Loading...</h1>
-        <Circles
-          height="80"
-          width="80"
-          color="#4fa94d"
-          ariaLabel="circles-loading"
-          wrapperStyle={{}}
-          wrapperClass=""
-          visible={true}
-        />
+
+  return (
+    <>
+      <div>
+        <h1>IDEAS JAR</h1>
       </div>
-    );*/
+      <ListIdeas ideas={jsonData} text={text} setText={setText} />
+    </>
+  );
 }
 export default App;
--- src/components/ListIdeas.jsx
+++ src/components/ListIdeas.jsx
@@ -1,65 +1,69 @@
 //import { motion } from "framer-motion";
-//import { useState } from "react";
-//import axios from "axios";
-//import ideas from "../data/jar.json";
-import ViewIdea from "./ViewIdea";
+import { useState } from "react";
 
-const ListIdeas = ({ ideas, view, setView, text, setText }) => {
-  //const [count, setCount] = useState(0);
-  //const [ideas, setIdeas] = useState([]);
-  // Try to get the jar.json file from the server
-  /*axios.get("http://ideajar.paranom.asia/api/getjson.php").then(
-    (response) => {
-      setIdeas(response.data);
-      setCount(ideas.length);
-      //console.log(response.data);
-    },
-    (error) => {
-      console.log(error);
-    }
-  );*/
+const ListIdeas = ({ ideas }) => {
   const count = ideas.length;
+  const initToggle = [];
+  for (let i = 0; i < count; i++) {
+    initToggle[i] = false;
+  }
+  const [toggle, setToggle] = useState(initToggle);
 
-  const handleClick = (idea) => () => {
-    setView(true);
-    setText(idea.description);
+  const clicked = (index) => {
+    setToggle((prevToggle) => {
+      const newToggle = [...prevToggle];
+      newToggle[index] = !newToggle[index];
+      return newToggle;
+    });
   };
+  //console.log(toggle);
 
-  if (view) return <ViewIdea text={text} setView={setView} />;
-  else
-    return (
-      <>
-        <div>
-          <>
-            {count == 0 && (
-              <div>
-                <h2 style={{ fontWeight: 400 }}>The Jar is empty!</h2>
-              </div>
-            )}
-            {count == 1 && (
-              <div>
-                <h2 style={{ fontWeight: 400 }}>
-                  There's only 1 idea in the Jar
-                </h2>
-              </div>
-            )}
-            {count > 1 && (
-              <div>
-                <h2 style={{ fontWeight: 400 }}>
-                  There are {count} ideas in the Jar
-                </h2>
-              </div>
-            )}
-            {ideas.map((idea, key) => (
-              <div className="idea" key={key}>
-                <button className="question-btn" onClick={handleClick(idea)}>
-                  ?
+  //  RENDER
+
+  return (
+    <>
+      <div>
+        <>
+          {count == 0 && (
+            <div>
+              <h2 style={{ fontWeight: 400 }}>The Jar is empty!</h2>
+            </div>
+          )}
+          {count == 1 && (
+            <div>
+              <h2 style={{ fontWeight: 400 }}>
+                There's only 1 idea in the Jar
+              </h2>
+            </div>
+          )}
+          {count > 1 && (
+            <div>
+              <h2 style={{ fontWeight: 400 }}>
+                There are {count} ideas in the Jar
+              </h2>
+            </div>
+          )}
+
+          {ideas.map((idea, index) => (
+            <div className="idea" key={index}>
+              {toggle[index] && (
+                <button className="idea-btn" onClick={() => clicked(index)}>
+                  {idea.description}
                 </button>
-              </div>
-            ))}
-          </>
-        </div>
-      </>
-    );
+              )}
+              {!toggle[index] && (
+                <button
+                  className="idea-btn-transparent"
+                  onClick={() => clicked(index)}
+                >
+                  {idea.description}
+                </button>
+              )}
+            </div>
+          ))}
+        </>
+      </div>
+    </>
+  );
 };
 export default ListIdeas;
